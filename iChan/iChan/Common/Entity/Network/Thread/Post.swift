4610372432c67d6e3fe19d06ef3fa00bb428b366
enoxus <reqenoxus@gmail.com>
2020-03-15 20:19:42 +0300

    added post entity

---
+++ iChan/iChan/Common/Entity/Network/Thread/Post.swift
@@ -0,0 +1,25 @@
+//
+//  Post.swift
+//  iChan
+//
+//  Created by Enoxus on 15/03/2020.
+//  Copyright © 2020 Enoxus. All rights reserved.
+//
+
+import Foundation
+
+struct Post: Codable {
+    
+    let comment: String
+    let name: String
+    let op: Int
+    let num: String
+    let date: String
+    let files: [File]
+}
+
+struct File: Codable {
+    
+    let path: String
+    let thumbnail: String
+}
